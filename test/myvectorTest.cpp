dda2d9e8107e1f118fbedfe9c98dbcdf9b93dd57
abdo <abdalrahmangamal1999@gmail.com>
2025-01-12 13:01:32 +0200

    solving problem of testing get capacity function

--- test/myvectorTest.cpp
+++ test/myvectorTest.cpp
@@ -1,41 +1,57 @@
 #include "myvector.hpp"
 #include <gtest/gtest.h>
 
-TEST(getingValue, get)
-{
-    EXPECT_EQ(55, get(1));
-    EXPECT_EQ(0, 0);
-    EXPECT_NE(58, get(1));
+Vector v={1,2,3,4,5};
+TEST(getingValue,size)
+{   
+
+    EXPECT_EQ(5,v.size());
+}
+TEST(pushingbackValue, push_back)
+{   v.push_back(10);
+    EXPECT_EQ(10, v.get_back());
 }
 
-TEST(getingValue, pop)
+TEST(pushingfrontValue,push_front)
 {
-    EXPECT_EQ(5, pop(1));
-    EXPECT_EQ(0, 0);
-    EXPECT_NE(58, pop(1));
+    v.push_front(0);
+    EXPECT_EQ(0,v.get(0));
 }
-TEST(insertingValue, push)
+
+TEST(insertingValue, insert)
 {
-    push_back(2);
-    EXPECT_EQ(5, get_back());
-    // check this
-    //  push_front()
-    //  EXPECT_NE();
+    v.insert(3,20);
+    EXPECT_EQ(20, v.get(3));
+  
 }
-// TODO: complete the following
-//  {
-//      expend_capacity();
-//      set(1, 5);
-//      size();
-//      get_front();
-//      find_index(25);
-//      print();
-//      right_rotate();
-//      left_rotate();
-//      right_rotate(6);
-//      push_front(9);
-//      insert(3, 8);
-//  }
+TEST(setingValue,set)
+{ 
+    v.set(0,40);
+    EXPECT_EQ(40,v.get_front());
+
+}
+TEST(popingValue,pop)
+{
+    EXPECT_EQ(10,v.pop(v.size()-1));
+}
+TEST(expandingcapacity,expandcapacity)
+{  
+    size_t size_prev=v.size();
+    v.expand_capacity();
+    EXPECT_EQ(size_prev*2,v.get_capacity());
+   
+}
+TEST(findingValue,find_index)
+{
+    EXPECT_EQ(40,v.find_index(0));
+}
+TEST(rotating,right_rotate)
+{
+    v.right_rotate();
+    EXPECT_EQ(40,v.get_back());
+    EXPECT_EQ(5,v.get_front());
+
+}
 int main()
 {
     testing::InitGoogleTest();
