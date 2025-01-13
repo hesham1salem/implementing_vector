6a63d853c8cf7e704b7eb31e8fae7f80addc9d5b
abdo <abdalrahmangamal1999@gmail.com>
2025-01-13 12:46:49 +0200

    add test cases for copy constructor and assignment and also add test cases for test move constructor and assignment

--- test/myvectorTest.cpp
+++ test/myvectorTest.cpp
@@ -81,3 +81,36 @@ TEST_F(myvector_test,left_rotate)
     EXPECT_EQ(2,v.get_front());
    
 }
+
+TEST_F(myvector_test,copy_constructor)
+{
+    Vector v1(v);
+    EXPECT_EQ(v.size(),v1.size());
+    EXPECT_EQ(v.get_capacity(),v1.get_capacity());
+}
+
+TEST_F(myvector_test,copy_assignment)
+{
+    Vector v1;
+    v1=v;
+    EXPECT_EQ(v.size(),v1.size());
+    EXPECT_EQ(v.get_capacity(),v1.get_capacity());
+}
+
+TEST_F(myvector_test,move_constructor)
+{   
+    size_t prev_size =v.size();
+    Vector v1=std::move(v);
+    EXPECT_EQ(prev_size,v1.size());
+    EXPECT_EQ(0,v.size());
+}
+
+TEST_F(myvector_test,move_assignment)
+{
+    size_t prev_size =v.size();
+    Vector v1;
+    v1=std::move(v);
+    EXPECT_EQ(prev_size,v1.size());
+    EXPECT_EQ(0,v.size());
+
+}
